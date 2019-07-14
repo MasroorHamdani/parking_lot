5cf3491083bede61d30ffb8af50ae0dc4480443d
Masroor Hamdani <masroorhamdani@yahoo.in>
2019-07-14 13:39:35 +0800

    test case flow

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 import sys, os
 
+from command_execute import CommandExecute
+
+command_execute_obj = CommandExecute()
+
+
+def main():
+    """
+    This function will handle the interaction part
+    Differentiates if the commands are passed
+    as a file or manually
+    """
+    try:
+        file_path = sys.argv[1] if len(sys.argv) >= 2 else ''
+        if file_path:
+            cmd_list = []
+            if os.path.isfile(file_path):
+                cmd_list = open(file_path, 'r')
+                command_execute_obj.execute_file(cmd_list)
+        else:
+            while True:
+                cmd = input("Press Enter the command..\n")
+                if cmd == 'exit':
+                    break
+                command_execute_obj.execute_command(cmd)
+    except Exception as e:
+        print("Error Occured {}".format(e))
+
 if __name__ == "__main__":
-    file_path = sys.argv[1] if len(sys.argv) >= 2 else ''
-    if file_path:
-        cmd_list = []
-        if os.path.isfile(file_path):
-            f = open(file_path, 'r')
-            for line in f:
-                cmd_list.append(line)
-    else:
-        while True:
-            command = input("Press Enter the command..\n")
-            if command == 'exit':
-                break
+    """
+    Entry point for our script
+    """
+    main()
